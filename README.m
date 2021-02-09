b933e70c906ba6d163e4de27b27aad979be5c92e
armandgeddon <72926482+armandgeddon@users.noreply.github.com>
2021-02-09 18:49:50 -0500

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,12 @@
+# derivadas direccionales 
+T=linspace(0,2*pi,50)
+t=deg2rad(T);
+x=2*e.^(2*t);
+y=cos(4*t);
+z=sin(4*t);
+plot3(x,y,z),hold on
+Fx=x;
+Fy=y;
+Fz=z;
+quiver3(x,y,z,Fx,Fy,Fz)
+plot3(1,2,2.87,"marker","*","markersize",100), hold on 
